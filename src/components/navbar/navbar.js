76823312e9a43bdfd31f4c7db6cf23625c8a4cab
Xavier Duncan <xduncan@pm.me>
2019-04-02 14:42:03 -0400

    add navbar background and logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+const NavbarStyle = {
+  margin: '0',
+  padding: '5px 15px',
+  color: '#FFF',
+  backgroundColor: '#000',
+}
+
 const Navbar = () =>
-  <div>
-    <h1>SimpleStock.</h1>
+  <div style={NavbarStyle}>
+    <h1><span role='img' aria-label='stock chart emoji'>💹 &nbsp;</span>SimpleStock</h1>
   </div>
 
 export default Navbar;
