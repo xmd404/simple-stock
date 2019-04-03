462c761608635472b8aa81233b14b4b915c6e1aa
Xavier Duncan <xduncan@pm.me>
2019-04-03 15:06:48 -0400

    add links to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,11 +7,33 @@ const NavbarStyle = {
   backgroundColor: '#000',
 };
 
+const MenuStyle = {
+  backgroundColor: 'yellow',
+};
+
+const LinkStyle = {
+  height: '100%',
+  display: 'inline-block',
+  padding: '1.25em 2.00em',
+};
+
 const Navbar = () =>
-  <div style={NavbarStyle}>
-    <h2>
-      <span role='img' aria-label='stock chart emoji'>💹 &nbsp;</span>SimpleStock
-    </h2>
+  <div>
+    <div style={NavbarStyle}>
+      <h2>
+        <span role='img' aria-label='money with wings emoji'>💸 &nbsp;</span>SimpleStock
+      </h2>
+    </div>
+    <div style={MenuStyle}>
+      <p style={LinkStyle}>Start Here</p>
+      <p style={LinkStyle}>Time & Money</p>
+      <p style={LinkStyle}>Stocks</p>
+      <p style={LinkStyle}>Forex</p>
+      <p style={LinkStyle}>Crypto</p>
+      <p style={LinkStyle}>Work Remote</p>
+      <p style={LinkStyle}>Live Abroad</p>
+      <p style={LinkStyle}>Pay Less Tax</p>
+    </div>
   </div>
 
 export default Navbar;
