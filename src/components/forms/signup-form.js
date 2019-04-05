c66d485d3fec3d37abcc91403cb58a3c8800b6d5
Xavier Duncan <xduncan@pm.me>
2019-04-05 17:38:58 -0500

    add signup form

--- src/components/forms/signup-form.js
+++ src/components/forms/signup-form.js
@@ -1,15 +1,69 @@
 import React from 'react';
 
 const formStyle = {
-  minHeight: '500px',
-  minWidth: '100%',
-  color: '#FFF',
+  margin: '0',
+  padding: '50px',
+  height: '265px',
+  width: 'auto',
+  color: '#fff',
+  backgroundColor: '#000',
+  textAlign: 'center',
+};
+
+const buttonStyle = {
+  margin: '0',
+  padding: '8px 82px',
+  borderRadius: '5px',
+  color: '#fff',
   backgroundColor: '#000',
 };
 
-const SignUpForm = () =>
-  <div style={formStyle}>
-    
-  </div>
+export default class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-export default SignUpForm;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    alert('Thanks for subscribing ' + this.state.value);
+  }
+
+  render() {
+    return (
+      <div style={formStyle}>
+        <h2><span role='img' aria-label='money with wings emoji'>💸 &nbsp;</span>SimpleStock</h2>
+        <p>Build wealth for your future self. Sign up below free access.</p>
+        <br/>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            &nbsp;
+            <input
+              type='text'
+              text='name'
+              placeholder='Alex Smith'
+              value={this.state.value}
+              onChange={this.handleChange} />
+          </label>
+          <br/><br/>
+          <label>
+            Email:
+            &nbsp;
+            <input type='text'text='emaul' placeholder='your@email.com' />
+          </label>
+          <br/><br/>
+          <input type='submit' value='Submit' style={buttonStyle} />
+          <br/><br/>
+        </form>
+      </div>
+    )
+  }
+};
