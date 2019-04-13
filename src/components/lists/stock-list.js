f8b8fb696c575a44e2c159b9c15bc8468c558462
Xavier Duncan <xduncan@pm.me>
2019-04-13 01:39:41 -0400

    refactor axios get requeset

--- src/components/lists/stock-list.js
+++ src/components/lists/stock-list.js
@@ -3,9 +3,12 @@ import axios from 'axios';
 import { error } from '../../utility';
 
 class StockList extends Component {
-  state = {
+  constructor(props){
+    super(props);
+    this.state = {
       tickets: [],
     };
+  }
 
   componentDidMount() {
     const API_KEY = 'pk_f8fb41e0cff74b73b0ad93bf4a374421';
@@ -15,7 +18,7 @@ class StockList extends Component {
       & symbols = aapl,fb,snap`
     )
     .then(response => {
-      console.log(response);
+      console.log(response.data);
       this.setState({ tickets: response.data });
     })
     .catch(error())
