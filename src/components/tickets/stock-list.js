1d5e3fdef3960c042c9252bb805d5c07c246ae28
Xavier Duncan <xduncan@pm.me>
2019-04-12 21:40:50 -0400

    refactor StockList component

---
+++ src/components/tickets/stock-list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { error } from '../../utility';
+
+class StockList extends Component {
+  state = {
+      tickets: [],
+      error: undefined,
+    };
+
+  componentDidMount() {
+    const API_KEY = 'pk_f8fb41e0cff74b73b0ad93bf4a374421';
+    axios.get(
+      `https://cloud.iexapis.com/beta/tops
+      ? token = ${API_KEY}
+      & symbols = aapl,fb,snap`
+    )
+    .then(response => {
+      console.log(response);
+      this.setState({ tickets: response.data });
+    })
+    .catch(error())
+  }
+
+  render() {
+    const { tickets } = this.state;
+    return (
+      <ul>
+        {tickets.map(ticket => (
+          <li key={ticket.symbol} ticket={ticket}>
+            {ticket.symbol}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+};
+
+export default StockList;
