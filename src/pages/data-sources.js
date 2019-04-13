50e9722e67490072e8d36e74595199443367df56
Xavier Duncan <xduncan@pm.me>
2019-04-13 02:45:23 -0400

    update data sources page

--- src/pages/data-sources.js
+++ src/pages/data-sources.js
@@ -1,15 +1,68 @@
 import React from 'react';
 
 const DataSourcePageStyle = {
-  margin: '0',
+  margin: '0 0 20% 0',
   padding: '15px 30px',
 };
 
 const DataSourcePage = () =>
   <div style={DataSourcePageStyle}>
-    <h1>
-      Data sources we use:
-    </h1>
+    <h1>Data sources:</h1>
+    <br />
+    <div>
+      <h2>IEX Cloud</h2>
+      <a 
+      href='https://iexcloud.io/'
+      target='_blank'
+      rel='noopener noreferrer'
+      >
+        Website
+      </a>
+      &nbsp; | &nbsp;
+      <a 
+      href='https://iexcloud.io/docs/api/'
+      target='_blank'
+      rel='noopener noreferrer'
+      >
+        Docs
+      </a>
+    </div>
+    <div>
+      <h2>Fixer</h2>
+      <a 
+      href='https://fixer.io/'
+      target='_blank'
+      rel='noopener noreferrer'
+      >
+        Website
+      </a>
+      &nbsp; | &nbsp;
+      <a 
+      href='https://fixer.io/documentation'
+      target='_blank'
+      rel='noopener noreferrer'
+      >
+        Docs
+      </a>
+    </div>
+    <div>
+      <h2>CoinMarketCap</h2>
+      <a 
+      href='https://coinmarketcap.com/'
+      target='_blank'
+      rel='noopener noreferrer'
+      >
+        Website
+      </a>
+      &nbsp; | &nbsp;
+      <a 
+      href='https://coinmarketcap.com/api/documentation/v1/'
+      target='_blank'
+      rel='noopener noreferrer'
+      >
+        Docs
+      </a>
+    </div>
   </div>
 
 export default DataSourcePage;
