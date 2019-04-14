7e1267abf360bb0ff2feb243f73ca2352c8c9e79
Xavier Duncan <xduncan@pm.me>
2019-04-14 11:24:13 -0400

    render API data to StockList

--- src/components/lists/stock-list.js
+++ src/components/lists/stock-list.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { error } from '../../utility';
 
+const ListStyle = {
+  overflowX: 'none',
+  margin: '0',
+  padding: '45px 25px',
+};
+
+const TicketStyle = {
+  display: 'inline-block',
+  margin: '0',
+  minWidth: '240px',
+  maxWidth: '100%',
+  padding: '5px 15px',
+  color: '#000',
+  backgroundColor: '#FFF',
+};
+
 class StockList extends Component {
   constructor(props){
     super(props);
@@ -12,13 +28,18 @@ class StockList extends Component {
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     axios.get(
-      `https://cloud.iexapis.com/beta/stock/market/batch?token=pk_f8fb41e0cff74b73b0ad93bf4a374421&symbols=aapl,fb,tsla,snap,googl&types=quote,news,chart&range=1m&last=5`
+      `https://cloud.iexapis.com/beta/stock/market/batch?token=${process.env.REACT_APP_STOCK_API_KEY}&symbols=aapl,fb,tsla,snap,googl,amzn, lyft&types=quote,news,chart&range=1m&last=5`
     )
-    .then(response => {
-      console.log(response.data);
-      this.setState({ tickets: response.data });
+    .then(res => {
+      let ticketsObj = res.data;
+      let ticketsArr = Object.values(ticketsObj);
+      this.setState({ 
+        tickets: ticketsArr,
+        isLoaded: true,
+      });
+      console.log(this.state.tickets);
     })
     .catch(error())
   }
@@ -31,13 +52,18 @@ class StockList extends Component {
       return <div>Stock list is Loading...</div>;
     } else {
       return (
-        <ul>
+        <div style={ListStyle}>
           {tickets.map(ticket => (
-            <li key={ticket.symbol} ticket={ticket}>
-              {ticket.symbol}
-            </li>
+            <div key={ticket.quote.symbol} ticket={ticket} style={TicketStyle}>
+              <hr/>
+              <p>{ticket.quote.companyName}</p>
+              <h1>{ticket.quote.symbol}</h1>
+              <p>Current: ${ticket.quote.latestPrice}</p>
+              <p>High: ${ticket.quote.week52High}</p>
+              <p>Low: ${ticket.quote.week52Low}</p>
+            </div>
           ))}
-        </ul>
+        </div>
       );
     }
   }
