291bfa7cd6fb99e097b1a36aa8a079f7038f4e80
Xavier Duncan <xduncan@pm.me>
2019-04-24 16:13:19 -0400

    add forex to homepage / flash briefing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import StockList from '../components/lists/stock-list';
 import News from '../components/lists/news-list';
+import ForexList from '../components/lists/forex-list';
 
 const HomePage = () =>
   <div>
     <News />
     <StockList />
+    <ForexList />
   </div>
 
 export default HomePage;
