315ff8ccceedbf2759463624705239d3a918ae43
Xavier Duncan <xduncan@pm.me>
2019-10-19 12:19:54 -0500

    update charts

--- src/components/charts/stock-chart.js
+++ src/components/charts/stock-chart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { error } from '../../utility';
+import { error, symbols, getMarketSymbol, getMarketMessage, Loading } from '../../utility';
 
 class StockChart extends Component {
   constructor(props) {
@@ -15,11 +15,13 @@ class StockChart extends Component {
   componentWillMount() {
     console.time('Fetching stock chart');
     axios.get(
-
+      `https://cloud.iexapis.com/beta/stock/market/batch?token=${process.env.REACT_APP_STOCK_API_KEY}&symbols=${symbols}&types=quote,news`
     )
     .then(response => {
+      let stockObj = response.dat
+      let stock = '';
       this.setState({
-        // stock: stock,
+        stock: stock,
         isLoaded: true,
       });
       console.timeEnd('Fetching stock chart');
