7288b578432faf039a574edf3e78f01020c52f55
Xavier Duncan <xduncan@pm.me>
2019-10-24 20:25:19 -0500

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import SignUpForm from '../components/forms/sign-up';
+
 
 const Container = styled.div`
-  height: 100%;
-  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 700px;
+  width: auto;
+  min-height: 100%;
+  color: #fff;
   background-color: #000;
   text-align: center;
+  font-size: 150%;
 `;
 
 const PageNotFound = () =>
   <Container>
-    <h1>
-      <SignUpForm />
-    </h1>
+    <br/><br/>
+    <h1 style={{ margin: '0', padding: '0' }}>404</h1>
+    <p>page not found, brought this though:</p>
+
   </Container>
 
 export default PageNotFound;
