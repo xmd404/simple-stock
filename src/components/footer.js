590ca0cba9b9212e96119d7167cc8719e9759bf7
Xavier Duncan <xduncan@pm.me>
2019-10-25 00:30:24 -0500

    refactor footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Foot = styled.footer`
-  margin: 0;
-  text-align: center;
+	width: auto;
+	margin: 0;
+	padding: 35px;
+	text-align: center;
 `;
 
-const Footer = () =>
-  <Foot>
-    <p>
-      Thanks for using
-      &nbsp;
-      <span role='img' aria-label='money with wings emoji'>💸 </span><b>SimpleStock</b>.
-      &nbsp;
-      <Link to='/data-sources' style={{ color: '#fff' }}>
-        Where do we get our data
-      </Link>?
-    </p>
-  </Foot>
+const Footer = () => (
+	<Foot>
+		<p>
+			Thanks for using &nbsp;
+			<span role="img" aria-label="money with wings emoji">
+				💸{' '}
+			</span>
+			<b>SimpleStock</b>. &nbsp;
+			<Link to="/data-sources" style={{ color: '#fff' }}>
+				Where do we get our data
+			</Link>?
+		</p>
+	</Foot>
+);
 
 export default Footer;
