b4784d580cb418820a2bfe4cef607f0cb5276049
Xavier Duncan <xduncan@pm.me>
2019-10-25 01:02:01 -0500

    update stock symbols

--- src/utility.js
+++ src/utility.js
@@ -1,57 +1,51 @@
-import React from "react";
+import React from 'react';
 import Loader from 'react-loader-spinner';
 
 // data used by exchange APIs
-export const symbols = 'aapl,fb,tsla,msft,lyft,twtr,uber,';
+export const symbols = 'hulu,nflx,dis,fb,tsla,twtr,lyft,uber,real,work,';
 export const pairs = 'USD,JPY,GBP,AUD,CAD,CHF,CNY,AED,NZD';
 
 // convert unix timestamp into regular dates
 export const getDateTime = (unix_timestamp) => {
-  let date = new Date(unix_timestamp * 1000);
-  let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  let month = months[date.getMonth()];
-  let day = date.getDate();
-  let timestamp = `${month} ${day}`;
-  return timestamp;
+	let date = new Date(unix_timestamp * 1000);
+	let months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+	let month = months[date.getMonth()];
+	let day = date.getDate();
+	let timestamp = `${month} ${day}`;
+	return timestamp;
 };
 
 //  message for markets open/closed
 export const getMarketMessage = (signal) => {
-  const statusMsg = signal === 'close' ?
-    `🛑 closed` :
-    `open 💹 `;
-  const status = `${statusMsg}`;
-  return status;
+	const statusMsg = signal === 'close' ? `closed 🛑` : `open 💹`;
+	const status = `${statusMsg}`;
+	return status;
 };
 
 // handle errors
 export const error = () => {
-  if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    console.log(error.response.data);
-    console.log(error.response.status);
-    console.log(error.response.headers);
-  } else if (error.request) {
-    // The request was made but no response was received
-    // `error.request` is an instance of XMLHttpRequest in the browser 
-    // and an instance of http.ClientRequest in node.js
-    console.log(error.request);
-  } else {
-    // // Something happened in setting up the request that triggered an Error
-    // console.log('Error', error.message);
-  }
-  // console.log(error.config);
+	if (error.response) {
+		// The request was made and the server responded with a status code
+		// that falls out of the range of 2xx
+		console.log(error.response.data);
+		console.log(error.response.status);
+		console.log(error.response.headers);
+	} else if (error.request) {
+		// The request was made but no response was received
+		// `error.request` is an instance of XMLHttpRequest in the browser
+		// and an instance of http.ClientRequest in node.js
+		console.log(error.request);
+	} else {
+		// // Something happened in setting up the request that triggered an Error
+		// console.log('Error', error.message);
+	}
+	// console.log(error.config);
 };
 
 // handle loading
-export const Loading = () => 
-  <div style={{textAlign: 'center', margin: '10em 5em'}}>
-    <Loader 
-      type="Rings"
-      color="fuchsia"
-      height={80}
-      width={80}
-    />
-    <p>Loading</p>
-  </div>
+export const Loading = () => (
+	<div style={{ textAlign: 'center', margin: '10em 5em' }}>
+		<Loader type="Rings" color="fuchsia" height={80} width={80} />
+		<p>Loading</p>
+	</div>
+);
