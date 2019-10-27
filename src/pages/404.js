637e2aea772a6bf580d6f90351a3e16d1bb6835a
Xavier Duncan <xduncan@pm.me>
2019-10-27 07:59:59 -0500

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,11 +5,16 @@ import styled from 'styled-components';
 const Container = styled.div`
   margin: 0;
   padding: 0;
-  height: 700px;
+  height: 600px;
   width: auto;
   min-height: 100%;
   text-align: center;
   font-size: 150%;
+`;
+
+const Image = styled.img`
+  maxheight: 500px;
+  width: auto;
 `;
 
 const PageNotFound = () =>
@@ -17,7 +22,7 @@ const PageNotFound = () =>
     <br/><br/>
     <h1 style={{ margin: '0', padding: '0' }}>404</h1>
     <p>page not found, brought this instead</p>
-
+    <Image src={'pigsfly.gif'} />
   </Container>
 
 export default PageNotFound;
