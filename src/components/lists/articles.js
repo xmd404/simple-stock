b6df644e06be198819417d0d9a3f91434291065b
xmd404 <xmd404@pm.me>
2020-12-07 06:00:30 -0600

    add loading and error states to articles section

--- src/components/lists/articles.js
+++ src/components/lists/articles.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Title, List, NewsCard, Headline, Thumbnail } from './components';
+import { error, symbols, Loading } from '../../utility';
 import axios from 'axios';
 
 const Articles = () => {
-    // const [hasError, setError] = useState(false);
-    // const [isLoaded, setLoaded] = useState(false);
+    const [error, setError] = useState(false);
+    const [isLoaded, setLoaded] = useState(false);
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
@@ -18,42 +19,47 @@ const Articles = () => {
                 console.log(err);
             })
     }, []);
-    
-    return (
-        <div>
-            {/* <Title>
-                <h1 style={{ margin: '0', padding: '0' }}>Articles</h1>
-            </Title> */}
-            <List
-                className="list-scroll"
-            >
-                {articles.map((article) => (
-                    <a
-                        key={article.url}
-                        article={article}
-                        href={article.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <NewsCard news>
-                            {/* <Thumbnail src={article.urlToImage} /> */}
-                            <Headline>
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <Loading />;
+    } else {
+        return (
+            <div>
+                {/* <Title>
+                    <h1 style={{ margin: '0', padding: '0' }}>Articles</h1>
+                </Title> */}
+                <List
+                    className="list-scroll"
+                >
+                    {articles.map((article) => (
+                        <a
+                            key={article.url}
+                            article={article}
+                            href={article.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <NewsCard news>
+                                {/* <Thumbnail src={article.urlToImage} /> */}
+                                <Headline>
+                                    <br/>
+                                    <b>
+                                        <p style={{ height: '100px' }}>{article.title.substring(0, 105)}</p>
+                                    </b>
+                                    {/* <p>{article.publishedAt.substring(0, 16).replace(regex, ' @ ')}</p> */}
+                                    <p>
+                                        <u>{article.source.name.toLowerCase()}</u>
+                                    </p>
+                                </Headline>
                                 <br/>
-                                <b>
-                                    <p style={{ height: '100px' }}>{article.title.substring(0, 105)}</p>
-                                </b>
-                                {/* <p>{article.publishedAt.substring(0, 16).replace(regex, ' @ ')}</p> */}
-                                <p>
-                                    <u>{article.source.name.toLowerCase()}</u>
-                                </p>
-                            </Headline>
-                            <br/>
-                        </NewsCard>
-                    </a>
-                ))}
-            </List>
-        </div>
-    );
+                            </NewsCard>
+                        </a>
+                    ))}
+                </List>
+            </div>
+        );
+    };
 };
 
 export default Articles;
