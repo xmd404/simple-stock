01ed18feee6078e3baa357b6da08acac8454f397
xmd404 <xmd404@pm.me>
2020-12-07 07:41:26 -0600

    make header logo link to homepage

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { getHomePage } from '../utility';
 
 const Logo = styled.header`
 	margin: 50px 0;
@@ -10,16 +12,18 @@ const Logo = styled.header`
 `;
 
 const Header = () => (
-	<Logo>
-		<h1 style={{ margin: '0', padding: '0' }}>
-			<span role="img" aria-label="money with wings emoji">
-				💸 &nbsp;
-			</span>SimpleStock
-		</h1>
-		<p style={{ margin: '0', padding: '0' }}>
-			Generational wealth, for all.
-		</p>
-	</Logo>
+	<Link to="/" onClick={getHomePage} style={{ textDecoration: 'none', color: '#fff' }}>
+		<Logo>
+			<h1 style={{ margin: '0', padding: '0' }}>
+				<span role="img" aria-label="money with wings emoji">
+					💸 &nbsp;
+				</span>SimpleStock
+			</h1>
+			<p style={{ margin: '0', padding: '0' }}>
+				Generational wealth, for all.
+			</p>
+		</Logo>
+	</Link>
 );
 
 export default Header;
