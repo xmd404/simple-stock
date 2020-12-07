40596f244590da54f12840253420179c6264e509
xmd404 <xmd404@pm.me>
2020-12-07 06:42:24 -0600

    refactor stocks list to react hooks

--- src/components/lists/stock.js
+++ src/components/lists/stock.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { List, Title, Card, Logo } from './components';
 import { error, symbols, getMarketMessage, getStockChart, Loading, shuffle } from '../../utility';
 import axios from 'axios';
 import Tips from '../miscellaneous/tips';
 
-class StockList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: null,
-			isLoaded: false,
-			stocks: [],
-		};
-	}
-
-	componentWillMount() {
-		console.clear();
-		console.time('Fetching stocks');
+const StockList = () => {
+	// set state
+	const [error, setError] = useState(false);
+	const [isLoaded, setLoaded] = useState(false);
+	const [stocks, setStocks] = useState([]);
+	const scrollRef = useRef("myscroll");
+	// fetch data from api
+	useEffect(() => {
 		axios.get(
 				`https://cloud.iexapis.com/beta/stock/market/batch?token=${process.env
 					.REACT_APP_STOCK_API_KEY}&symbols=${symbols}&types=quote,news`
@@ -24,53 +19,50 @@ class StockList extends Component {
 			.then((response) => {
 				let unsorted = Object.values(response.data);
 				let stocks = shuffle(unsorted);
-				this.setState({
-					stocks: stocks,
-					isLoaded: true,
-				});
-				console.timeEnd('Fetching stocks');
-				console.log({ stocks }, response.status);
+				setStocks(stocks);
+				setLoaded(true);
 			})
-			.catch(error());
-	}
+			.catch(err => {
+				setError(true);
+				console.log(err);
 
-	render() {
-		const { error, isLoaded, stocks } = this.state;
-		if (error) {
-			return <div>Error: {error.message}</div>;
-		} else if (!isLoaded) {
-			return <Loading />;
-		} else {
-			return (
-				<div>
-					<br/>
-					<p style={{ margin: '0', padding: '0', textAlign: 'center'}}>
-						Markets are&nbsp;
-						<b>{getMarketMessage(stocks[0].quote.calculationPrice)}</b>.
-					</p>
-					<Tips />
-					<Title>
-						<h1 style={{ margin: '0', padding: '0' }}>Stocks</h1>
-					</Title>
-					<List
-						className="list-scroll"
-						ref="myscroll"
-					>
-						{stocks.map((stock) => (
-							<Card key={stock.quote.symbol} stock={stock} onClick={getStockChart}>
-								<Logo src={`https://storage.googleapis.com/iex/api/logos/${stock.quote.symbol}.png`} />
-								<b>
-									<h2 className="cardTicker">{stock.quote.symbol.toLowerCase()}</h2>
-								</b>
-								<p style={{ height: '75px'}}>{stock.quote.companyName.toLowerCase().split(', inc.')}</p>
-								<h3>${stock.quote.latestPrice}</h3>
-							</Card>
-						))}
-					</List>
-				</div>
-			);
-		}
-	}
-}
+			});
+	}, []);
+	// render error, loading, or success state
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <Loading />;
+	} else {
+		return (
+			<div>
+				<br/>
+				<p style={{ margin: '0', padding: '0', textAlign: 'center'}}>
+					Markets are&nbsp;
+					<b>{getMarketMessage(stocks[0].quote.calculationPrice)}</b>.
+				</p>
+				<Tips />
+				<Title>
+					<h1 style={{ margin: '0', padding: '0' }}>Stocks</h1>
+				</Title>
+				<List
+					className="list-scroll"
+					ref={scrollRef}
+				>
+					{stocks.map((stock) => (
+						<Card key={stock.quote.symbol} stock={stock} onClick={getStockChart}>
+							<Logo src={`https://storage.googleapis.com/iex/api/logos/${stock.quote.symbol}.png`} />
+							<b>
+								<h2 className="cardTicker">{stock.quote.symbol.toLowerCase()}</h2>
+							</b>
+							<p style={{ height: '75px'}}>{stock.quote.companyName.toLowerCase().split(', inc.')}</p>
+							<h3>${stock.quote.latestPrice}</h3>
+						</Card>
+					))}
+				</List>
+			</div>
+		);
+	};
+};
 
 export default StockList;
