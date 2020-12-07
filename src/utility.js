65f9ea112a5ca01041bee8c158ea7e8d4f92e6bb
xmd404 <xmd404@pm.me>
2020-12-07 07:55:27 -0600

    add snow to app for 5 secs onload

--- src/utility.js
+++ src/utility.js
@@ -143,3 +143,38 @@ export const getProfilePage = () => {
 	);
 	console.log("Went to ProfilePage");
 };
+
+// snow
+
+var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var skew = 1;
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+  var timeLeft = animationEnd - Date.now();
+  var ticks = Math.max(200, 500 * (timeLeft / duration));
+  skew = Math.max(0.8, skew - 0.001);
+
+  window.confetti({
+    particleCount: 1,
+    startVelocity: 0,
+    ticks: ticks,
+    gravity: 0.5,
+    origin: {
+      x: Math.random(),
+      // since particles fall down, skew start toward the top
+      y: (Math.random() * skew) - 0.2
+    },
+    colors: ['#ffffff'],
+    shapes: ['circle'],
+    scalar: randomInRange(0.4, 1)
+  });
+
+  if (timeLeft > 0) {
+    requestAnimationFrame(frame);
+  }
+}());
