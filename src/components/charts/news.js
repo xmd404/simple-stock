514ea18de36bdc08968239d28e6c7df4572816b8
xmd404 <xmd404@pm.me>
2020-12-08 17:49:40 -0600

    convert news chart to react hooks

--- src/components/charts/news.js
+++ src/components/charts/news.js
@@ -1,79 +1,71 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { List, NewsCard, Headline, Thumbnail } from './components';
 import { error, symbols, Loading } from '../../utility';
 import axios from 'axios';
 
-class NewsChart extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: null,
-			isLoaded: false,
-            articles: [],
-            ticker: window.location.href.split("/")[5],
-		};
-    }
-
-	componentDidMount() {
-		console.time('Fetching news');
-		axios.get(
-				`https://stocknewsapi.com/api/v1?tickers=${window.location.href.split("/")[6]}&items=10&token=${process.env.REACT_APP_NEWS_API_KEY}`)
-			.then((response) => {
-				let articles = Object.values(response.data);
-				this.setState({
-					articles: articles,
-					isLoaded: true
+const NewsChart = () => {
+	// set state
+	const [error, setError] = useState(false);
+	const [isLoaded, setLoaded] = useState(false);
+	const [articles, setArticles] = useState([]);
+	const [ticker, setTicker] = useState(window.location.href.split("/")[5]);
+	const scrollRef = useRef("myscroll");
+	// fetch data from api
+	useEffect(() => {
+		axios
+			.get(`https://stocknewsapi.com/api/v1?tickers=${window.location.href.split("/")[6]}&items=10&token=${process.env.REACT_APP_NEWS_API_KEY}`)
+				.then(res => {
+					let articles = Object.values(res.data);
+					setArticles(articles);
+					setLoaded(true);
+				})
+				.catch(err => {
+					console.log(err);
+					setError(true);
 				});
-				console.timeEnd('Fetching news');
-				console.log({ articles }, response.status);
-			})
-			.catch(error());
-	}
-
-	render() {
-		const { error, isLoaded, articles } = this.state;
-		if (error) {
-			return <div>Error: {error.message}</div>;
-		} else if (!isLoaded) {
-			return <Loading />;
-		} else {
-			return (
-				<div>
-					<List
-						className="list-scroll"
-						ref="myscroll"
-					>
-						{articles[0].map((article) => (
-							<a
-								key={article.date}
-								article={article}
-								href={article.news_url}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<NewsCard news>
-									<Thumbnail src={article.image_url} />
-									<Headline>
-										<br/>
-										<p>{article.date.substring(0, 16)}</p>
-										<br/>
-										<b>
-											<p style={{ height: '100px' }}>{article.title.substring(0, 105)}</p>
-										</b>
-										<br/>
-										<p>
-											<u>{article.source_name.toLowerCase()}</u>
-										</p>
-									</Headline>
+	});
+	// render error, loading, or success state
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <Loading />;
+	} else {
+		return (
+			<div>
+				<List
+					className="list-scroll"
+					ref={scrollRef}
+				>
+					{articles[0].map((article) => (
+						<a
+							key={article.date}
+							article={article}
+							href={article.news_url}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<NewsCard news>
+								<Thumbnail src={article.image_url} />
+								<Headline>
 									<br/>
-								</NewsCard>
-							</a>
-						))}
-					</List>
-				</div>
-			);
-		}
-	}
-}
+									<p>{article.date.substring(0, 16)}</p>
+									<br/>
+									<b>
+										<p style={{ height: '100px' }}>{article.title.substring(0, 105)}</p>
+									</b>
+									<br/>
+									<p>
+										<u>{article.source_name.toLowerCase()}</u>
+									</p>
+								</Headline>
+								<br/>
+							</NewsCard>
+						</a>
+					))}
+				</List>
+			</div>
+		);
+	};
+;}
 
 export default NewsChart;
