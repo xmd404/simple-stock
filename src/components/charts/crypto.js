f6f44fc18daad3f6ba8ad8da47dd8f092968f0ce
xmd404 <xmd404@pm.me>
2020-12-08 18:05:05 -0600

    refactor crypto chart to hooks

--- src/components/charts/crypto.js
+++ src/components/charts/crypto.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { List, Title, Card, Logo } from './components';
 import { error, symbols, getCryptoChart, Loading } from '../../utility';
 import { ViewMoreButton } from '../miscellaneous/buttons';
@@ -16,61 +16,51 @@ const ticker = {
 // 	borderRadius: '5%'
 // };
 
-class CryptoChart extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: null,
-			isLoaded: false,
-			crypto: '',
-		};
-	}
-
-	componentDidMount() {
-		console.time('Fetching cryptos');
-		axios.get(
-      `https://api.coingecko.com/api/v3/coins/${window.location.href.split("/")[6]}
-      `)
-        .then((response) => {
-            let crypto = response.data;
-            this.setState({
-                crypto: crypto,
-                isLoaded: true
-            });
-            console.timeEnd('Fetching single crypto');
-            console.log({ crypto }, response.status);
-        })
-        .catch(error());
-	}
-
-	render() {
-		const { error, isLoaded, crypto } = this.state;
-		if (error) {
-			return <div>Error: {error.message}</div>;
-		} else if (!isLoaded) {
-			return <Loading />;
-		} else {
-			return (
-				<div>
-                    <Card key={crypto.symbol} crypto={crypto}>
-                        <div style={{ float: 'left', width: '50%' }}>
-                            <Logo src={crypto.image.thumb}/>
-                            <b>
-                                <h2>{crypto.symbol}</h2>
-                            </b>
-                            <p className="cardTicker" style={{ height: '75px'}}>{crypto.id}</p>
-                        </div>
-                        <div style={{ float: 'right', width: '50%', textAlign: 'right' }}>
-                            <br/>
-							<h2>${crypto.market_data.current_price.usd}</h2>
-                        </div>
-                    </Card>
-					<News />
-					<ViewMoreButton />
-				</div>
-			);
-		}
-	}
-}
+const CryptoChart = () => {
+	// set state
+	const [error, setError] = useState(false);
+	const [isLoaded, setLoaded] = useState(false);
+	const [crypto, setCrypto] = useState('');
+	// fetch data from api
+	useEffect(() => {
+		axios
+			.get(`https://api.coingecko.com/api/v3/coins/${window.location.href.split("/")[6]}`)
+				.then(res => {
+					let crypto = res.data;
+					setCrypto(crypto);
+					setLoaded(true);
+				})
+				.catch(err => {
+					console.log(err);
+					setError(true);
+				});
+	});
+	// render error, loading, or success state
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <Loading />;
+	} else {
+		return (
+			<div>
+				<Card key={crypto.symbol} crypto={crypto}>
+					<div style={{ float: 'left', width: '50%' }}>
+						<Logo src={crypto.image.thumb}/>
+						<b>
+							<h2>{crypto.symbol}</h2>
+						</b>
+						<p className="cardTicker" style={{ height: '75px'}}>{crypto.id}</p>
+					</div>
+					<div style={{ float: 'right', width: '50%', textAlign: 'right' }}>
+						<br/>
+						<h2>${crypto.market_data.current_price.usd}</h2>
+					</div>
+				</Card>
+				<News />
+				<ViewMoreButton />
+			</div>
+		);
+	};
+};
 
 export default CryptoChart;
