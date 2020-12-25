894d83f675befb62f0154089d38d664981a082ca
xmd404 <xmd404@pm.me>
2020-12-25 10:21:34 -0600

    add prod sotck api

--- src/components/charts/StockChartContainer.js
+++ src/components/charts/StockChartContainer.js
@@ -17,7 +17,7 @@ const StockChartContainer = () => {
 	// fetch data from api
 	useEffect(() => {
 		axios
-			.get(testStockChart)
+			.get(getStockChart)
 				.then(res => {
 					setStocks(Object.values(res.data));
 					setLoaded(true);
