88cda87ccec9388436f873bc73a9747a86bf55ec
xmd404 <xmd404@pm.me>
2020-12-31 21:09:14 -0600

    remove border from show/hide player

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import { ViewerPlayer, ViewerWindow } from './components';
 import Iframe from '@trendmicro/react-iframe';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
 
 const Viewer = () => {
-    const [toggle, setToggle] = useState('[-]  Hide Player')
+    const [toggle, setToggle] = useState('Hide Player')
     const toggleViewer = () => {
         let toggle;
         var x = document.getElementById("player");
         if (x.style.display === "none") {
           x.style.display = "inline-block";
-          setToggle("[-]  Hide Player");
+          setToggle("Hide Player");
         } else {
           x.style.display = "none";
-          setToggle("[+]  Show Player");
+          setToggle("Show Player");
         };
         return toggle;
     };
@@ -20,11 +25,12 @@ const Viewer = () => {
     return (
         <ViewerWindow >
             <br/>
-            <a onClick={toggleViewer} style={{ cursor: `pointer` }}>{toggle}</a>
+            <a onClick={toggleViewer} style={{ cursor: `pointer`, border: `none` }}><FontAwesomeIcon icon={["fab", "youtube"]} /> &nbsp; {toggle}</a>
             <br/>
             <div id="player">
                 <br/>
                 <ViewerPlayer src="https://www.youtube.com/embed/_PXFVNWINQc" />
+                <br/>
             </div>
             <br/>
         </ViewerWindow>
