70d6124c760c21e15434ddd5556ff48302b83332
xmd404 <xmd404@pm.me>
2021-01-05 23:02:55 -0600

    add padding to search container

--- src/components/search.js
+++ src/components/search.js
@@ -8,7 +8,7 @@ library.add(fas);
 
 const SearchBar = () => {
     const [text, setText] = useState('')
-    const BarStyling = {width:"95%", maxWidth:"750px", background:"#F2F1F9", border:"none", padding:"1rem 1rem", margin:"0 auto"};
+    const BarStyling = {width:"100%", maxWidth:"750px", background:"#F2F1F9", border:"none", padding:"1rem 1rem", margin:"0 auto"};
     
     let searchFilter = () => {
         let input, filter, ul, li, a, i, txtValue;
@@ -27,7 +27,7 @@ const SearchBar = () => {
     };
     
     return (
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center', padding: '0 20px' }}>
             <input 
                 style={BarStyling}
                 key="random1"
